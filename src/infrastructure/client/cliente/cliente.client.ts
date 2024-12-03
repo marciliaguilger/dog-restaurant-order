544df2ff6ef59248f26b841b1aa85745216f593b
marcilia.guilger@ifood.com.br <marcilia.guilger@ifood.com.br>
2024-12-03 20:59:12 -0300

    fi

--- src/infrastructure/client/cliente/cliente.client.ts
+++ src/infrastructure/client/cliente/cliente.client.ts
@@ -10,15 +10,19 @@ export class ClienteClient implements IClienteClient {
   constructor(private readonly httpService: HttpService) {}
   async getCliente(clientId: string): Promise<Cliente> {
     const url = `/clientes/${clientId}`;
-
-    const { data } = await firstValueFrom(
-      this.httpService.get<Cliente>(url).pipe(
-        catchError((error: AxiosError) => {
-          throw new HttpException(error.response.data, error.response.status);
-        }),
-      ),
-    );
-
-    return data;
+    try{
+      const { data } = await firstValueFrom(
+        this.httpService.get<Cliente>(url).pipe(
+          catchError((error: AxiosError) => {
+            throw new HttpException(error.response.data, error.response.status);
+          }),
+        ),
+      );
+      return data;
+    }catch(err){
+      console.log(err)
+      return undefined;
+    }
+    
   }
 }
